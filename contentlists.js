dfd016ecb6afabd563734e92c3f7f79dc56b4c62
PaulTestingGround <84423298+PaulTestingGround@users.noreply.github.com>
2021-06-02 21:43:58 -0400

    Add files via upload

--- contentlists.js
+++ contentlists.js
@@ -20,7 +20,7 @@ let characterTraits = [
     'You are learned and worldly, unlike the lot amongst whom you find yourself.',
     'You know very well that you are smooth, charming, and silver-tongued.',
     'You worry that your heart is dark and rotten.',
-    'You are filled with lightness, joy, and hope for the future, despite all that has passed.',
+    'You are filled with lightness, joy, and hope for the future, despite all that has come to pass.',
     'Death makes her home in your heart... none will challenge you without regretting it.',
     'You worry that you are losing your touch.',
     'You fear that you may yet come unhinged.',
@@ -38,6 +38,25 @@ let characterTraits = [
     'It is only your relentless vigilance that keeps you and your companions alive.',
     'Subtlety and stealth, that is the way to the top of the mountain.',
     'You are not who you seem.'
+]
+
+let specialTraits = [
+    'Perhaps you are an exiled noble, trying to regain your honour.',
+    'Perhaps you are an exotic foreigner: lost in a storm, your ship wrecked upon these shores.',
+    'Perhaps you are an assistant to an Imperial researcher, sent here on a quest to collect data for the Imperial Library; if so, you might carry a letter of proof and a leatherskin sketchbook.',
+    'Perhaps you are the heir to a major throne, on a quest to prove yourself before you return home to claim it.',
+    'Perhaps you are a barbarian prince or princess, far from home, heir to a proud but waning heritage.',
+    'Perhaps you are the last of your kind...',
+    'They whispered that you were the offspring of a divine being, only half mortal.',
+    'It is rumoured that you were the result of a virgin birth.',
+    'You also have a rare and special expertise, as an engineer: you know a lot about siege weaponry, fortifications, stone-masonry, tunneling, and map-making.',
+    'You once created an artwork, or wrote a book, which has since taken on a life of its own.',
+    'You have an excellent relationship with the region\'s most notable priest, priestess, shaman, prophet, or oracle.',
+    'You know a famous craftsman, craftswoman, or inventor.',
+    'You know a loremaster, who may divulge or research useful information.',
+    'You are on good terms with someone powerful: perhaps the local Lord, Lady, Commander, or Mayor.',
+    'You are the descendant of a renowned warlord or tyrant, killer of many.',
+    'You know a highly-placed member of the Conclave, a mysterious and powerful organization that controls occult knowledge.'
 ]
 
 let basicEquipment = [
@@ -97,7 +116,19 @@ let basicWeapons = [
     'a dagger',
     'a sickle',
     'a scythe',
-    'a whip'
+    'a whip or scourge',
+    'a wooden club',
+    'a farmer\'s flail',
+    'a hammer',
+    'a hatchet',
+    'a hunter\;s bow (and a dozen arrows)',
+    'a pitchfork',
+    'a wooden staff',
+    'a wood-splitting axe',
+    'a boning knife',
+    'a hunting knife',
+    'a stiletto',
+    'a short spear',
 ]
 
 let seriousWeapons = [
